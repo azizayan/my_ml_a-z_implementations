4fb6d651bd693ada2bde17e22cc5c73d951d49c4
azizayan <emre.ayan@ug.bilkent.edu.tr>
2026-02-10 23:11:34 +0300

    finish kNN implementation

--- algorithms_from_scratch/kNN/kNN.py
+++ algorithms_from_scratch/kNN/kNN.py
@@ -33,21 +33,6 @@ def euclidean_calculator(list1: List[float], list2: List[float]):
 
     return math.sqrt(result)
 
-
-def manhattan_calculator(list1: List[float], list2: List[float]):
-    length = len(list2)
-
-    result = 0.0
-
-    for i in range(length):
-        num1 = list1[i]
-        num2 = list2[i]
-
-        distance = math.abs(num1-num2)
-        result += distance
-
-    return result
-
 def load_csv(filename: Path):
     dataset = []
     with open(filename,'r') as file:
@@ -61,17 +46,83 @@ def load_csv(filename: Path):
             if not row:
                 continue
 
+            cleaned_row = []
+
+            for i in range(4):
+                cleaned_row.append(float(row[i]))
+
+            cleaned_row.append(row[4])
+
+            dataset.append(cleaned_row)
+
+    return dataset
+
+manual_dataset = load_csv('iris.csv')
+
+
             
+from sklearn.model_selection import train_test_split
+
+train_set, test_set = train_test_split(manual_dataset, test_size= 0.2, random_state=0)
 
         
 
 def get_neighbors(train_data, test_row, num_neighbors):
-    
+    distances = []
 
+    test_features = test_row[:-1]
+
+    for row in train_data:
+        train_features = row[:-1]
         
 
-        
+        distance = euclidean_calculator(train_features, test_features)
+        distances.append((row,distance))
+
+    distances.sort(key= lambda x:x[1])
+    neighbors = [t[0] for t in distances[:num_neighbors]]
+    return neighbors
     
 
+def predict_classification(train, test_row, num_neighbors):
+
+    neighbors = get_neighbors(train,test_row,num_neighbors)
+    labels = []
+
+    for neighbor in neighbors:
+        labels.append(neighbor[-1])
+
+    most_common_label = max(set(labels), key=labels.count)
+
+    return most_common_label
+
+def get_accuracy(train, test, num_neighbors):
+    if not test:
+        return 0.0
+
+
+    true_counter = 0
     
+    for row in test:
+        predicted = predict_classification(train,row, num_neighbors)
+        actual = row[-1]
+        if predicted == actual:
+            true_counter += 1
+    
+
+    return(true_counter/len(test))
+
+def main():
+    k = 3
+    accuracy = get_accuracy(train_set, test_set, k)
+    print(f"Accuracy (k={k}): {accuracy * 100:.2f}%")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
 
